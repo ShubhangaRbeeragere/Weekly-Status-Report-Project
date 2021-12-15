07cf69d46294d5625ccf83526dfad3e477c1cfe5
Shubhanga R B <shubhangarb@gmail.com>
2021-12-15 18:05:19 +0530

    added previousEmployment-service.ts/interface.ts

---
+++ src/interface/previousEmployement.interface.ts
@@ -0,0 +1,6 @@
+export interface employment{
+    companyName: string,
+    position: string,
+    startDate: string,
+    endDate: string
+}
---
+++ src/service/previousEmployment.service.ts
@@ -0,0 +1,3 @@
+import {getManager} from "typeorm"
+import PersonalData from "../model/entity/personalData";
+import * as employmentLayout from "../interface/previousEmployement.interface"
