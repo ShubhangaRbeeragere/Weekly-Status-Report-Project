f1e185bc6ad1edf74e57623d274330e4c816bcf3
Shubhanga R B <shubhanga.rb@travancoreanalytics.com>
2021-12-17 18:03:47 +0530

    not this repo

--- src/init/router.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import applicationDetails from "../router/applicantDetails.router"
-
-let mainRouter = express.Router();
-mainRouter.use("/applicantDetails", applicationDetails);
-
-export default mainRouter;
-
